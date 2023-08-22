6c34656e6491fc95830d3a75513f108cbc641b5f
Aidosmf <aidosmf@gmail.com>
2023-08-22 12:17:53 +0600

    chore: add & config size-limit reporting (#25)

---
+++ .size-limit.cjs
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    path: 'packages/last/dist/index.js',
+  },
+  {
+    path: 'packages/last-builder/dist/index.js',
+  },
+  {
+    path: 'packages/relottie/dist/index.js',
+  },
+  {
+    path: 'packages/relottie-parse/dist/index.js',
+  },
+  {
+    path: 'packages/relottie-stringify/dist/index.js',
+  },
+];
